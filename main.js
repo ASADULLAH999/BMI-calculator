fb889f78767e7949555e05648fdd2c62a8bb79de
ASADULLAH999 <162815522+ASADULLAH999@users.noreply.github.com>
2024-04-27 02:19:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env/node
+// simple BMI calculator:
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        message: "enter your weight",
+        type: "number",
+        name: "firstName"
+    },
+    {
+        message: "enter your height",
+        type: "number",
+        name: "secondName"
+    }
+]);
+let bmi = answer.firstName / (answer.secondName * answer.secondName);
+console.log(`your BMI is ${bmi}`);
